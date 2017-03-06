80bfc64d4131982b25df6021d29a897ed3d31d2a
Jerry Krusinski <jkruzr@gmail.com>
2017-03-06 12:33:55 -0600

    added error handling route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
-global.Promise = require('bluebird');
 
 var config = require('../config');
 var express = require('express');
 var mongoose = require('mongoose');
+
+var users = require('./routes/users');
+
+global.Promise = mongoose.Promise = require('bluebird');
 
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
@@ -17,8 +20,20 @@ app.use(bodyParser.urlEncoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client')));
 
+app.use('/api/users', users);
+
 app.get('/', function(req, res, next) {
   res.send('The server is running');
+});
+
+// handle errors
+app.use(function (err, req, res) {
+  console.log(err.message);
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: config.env === 'dev' ? err : ''
+  });
 });
 
 app.listen(config.port || 3000, function() {
