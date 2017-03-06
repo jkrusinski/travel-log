0621bc9f69f99849929178f6d4cf3c32cd9b3f28
Jerry Krusinski <jkruzr@gmail.com>
2017-03-06 15:10:38 -0600

    added passport to server index

--- server/index.js
+++ server/index.js
@@ -9,33 +9,49 @@ global.Promise = mongoose.Promise = require('bluebird');
 
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var passport = require('passport');
 
 var path = require('path');
 
+// init server and database connection
 var app = express();
 var db = mongoose.connect(config.database);
 
+// passport requirements
+app.use(expressSession({ secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// basic middleware
 app.use(morgan('dev'));
-app.use(bodyParser.urlEncoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client')));
 
-app.use('/api/users', users);
+// initialize passport
+require('./passport/init')(passport);
 
-app.get('/', function(req, res, next) {
-  res.send('The server is running');
+// routes
+app.use('/api/users', users(passport));
+
+// error handlers
+app.use(function(req, res, next) {
+  var err = new Error('Not found.');
+  err.status = 404;
+  next(err);
 });
 
-// handle errors
-app.use(function (err, req, res) {
+app.use(function (err, req, res, next) {
   console.log(err.message);
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: config.env === 'dev' ? err : ''
+    error: config.env === 'dev' ? err.stack : ''
   });
 });
 
+// listen
 app.listen(config.port || 3000, function() {
   console.log(`Server listening on port ${config.port || 3000}.`);
 });
