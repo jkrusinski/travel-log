953e589cc21e7b8d873daa87ef5969b8f01b1900
Jerry Krusinski <jkruzr@gmail.com>
2017-03-07 10:29:35 -0600

    added places route

--- server/routes/places.js
+++ server/routes/places.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+var Place = require('../models/Place');
+
 router.get('/', function(req, res, next) {
-  res.json(req.user);
+
+  Place.find({ user: req.user._id })
+
+  .then(function(places) {
+    res.json(places);
+  })
+
+  .catch(function(err) {
+    next(err);
+  });
+
 });
 
 router.post('/', function(req, res, next) {
+  var properties = ['placeId', 'trip', 'name', 'date', 'description'];
+  var newPlace = _.pick(req.body, properties);
+  newPlace.user = req.user._id;
 
+  Place.create(newPlace)
+
+  .then(function(place) {
+    res.json({ id: place._id });
+  })
+
+  .catch(function(err) {
+    next(err);
+  });
 });
 
 module.exports = router;
