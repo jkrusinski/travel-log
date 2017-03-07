a9c3ec89c8a06c47662221a76b1b9c6ae0b22af3
Jerry Krusinski <jkruzr@gmail.com>
2017-03-07 11:41:07 -0600

    send info back on user requests

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,18 +5,23 @@ var User = require('../models/User');
 
 var helpers = require('../helpers');
 
+var sendUserInfo = function(req) {
+  var properties = ['username', 'firstName', 'lastName', 'email'];
+  return _.pick(req.user, properties);
+};
+
 module.exports = function(passport) {
 
   // allows angularjs to check for valid session
   router.get('/', helpers.isAuth, function(req, res, next) {
-    res.sendStatus(200);
+    res.json(sendUserInfo(req));
   });
 
   router.post(
     '/login',
     passport.authenticate('login'),
     function(req, res, next) {
-      res.sendStatus(200);
+      res.json(sendUserInfo(req));
     }
   );
 
@@ -24,7 +29,7 @@ module.exports = function(passport) {
     '/signup',
     passport.authenticate('signup'),
     function(req, res, next) {
-      res.sendStatus(200);
+      res.json(sendUserInfo(req));
     }
   );
 
