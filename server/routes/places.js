315b342fcb2d9dd1235152f8ef2b7da5b5185508
Jerry Krusinski <jkruzr@gmail.com>
2017-03-07 11:01:26 -0600

    completed RESTful places routes

--- server/routes/places.js
+++ server/routes/places.js
@@ -16,6 +16,19 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:id', function(req, res, next) {
+
+  Place.findById(req.params.id)
+
+  .then(function(place) {
+    res.json(place);
+  })
+
+  .catch(function(err) {
+    next(err);
+  });
+});
+
 router.post('/', function(req, res, next) {
   var properties = ['placeId', 'trip', 'name', 'date', 'description'];
   var newPlace = _.pick(req.body, properties);
@@ -24,7 +37,22 @@ router.post('/', function(req, res, next) {
   Place.create(newPlace)
 
   .then(function(place) {
-    res.json({ id: place._id });
+    res.json(place);
+  })
+
+  .catch(function(err) {
+    next(err);
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  var properties = ['placeId', 'trip', 'name', 'date', 'description'];
+  var updated = _.pick(req.body, properties);
+
+  Place.findByIdAndUpdate(req.params.id, updated)
+
+  .then(function(updated) {
+    res.json(updated);
   })
 
   .catch(function(err) {
